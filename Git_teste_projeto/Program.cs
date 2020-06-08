0b67d8c76939956a37b69b30346971a7b7252d18
Aires <bassnuno@gmail.com>
2020-06-08 13:15:25 +0100

    Acrescentada mensgem de como estas hoje?

--- Git_teste_projeto/Program.cs
+++ Git_teste_projeto/Program.cs
@@ -4,6 +4,9 @@ namespace Git_teste_projeto {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Como voce esta hoje?");
+
             Console.ReadLine();
         }
     }
