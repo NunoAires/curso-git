03d98d407610fbbe9abee35049448f29c1ea97e0
Aires <bassnuno@gmail.com>
2020-06-08 12:59:36 +0100

    Projeto iniciado mostrando ola mundo

---
+++ Git_teste_projeto/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Git_teste_projeto {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
