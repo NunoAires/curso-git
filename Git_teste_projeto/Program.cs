945eec398b31f412ded1b7acbfb3eb8451f55aed
Aires <bassnuno@gmail.com>
2020-06-08 13:38:52 +0100

    Acrencetada mensagem tenha um bom dia

--- Git_teste_projeto/Program.cs
+++ Git_teste_projeto/Program.cs
@@ -4,9 +4,11 @@ namespace Git_teste_projeto {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+        
             Console.WriteLine("Como voce esta hoje?");
-            Console.WriteLine("Como voce esta hoje?");
+
+            Console.WriteLine(  "TEnha um bom dia");
+          
 
             Console.ReadLine();
         }
